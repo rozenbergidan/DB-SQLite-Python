0cb7e2ab1f22d18611902185fc841f83b5d99ab8
rozenbergidan <idanroze@post.bgu.ac.il>
2021-01-11 15:32:51 +0200

    added repo code

---
+++ PersistanceLayer/ClinicDAO.py
@@ -0,0 +1,9 @@
+class ClinicDAO:
+    def __init__(self,conn):
+        self._conn=conn
+
+    def get_clinic(self, clinic_id):
+        cur = self._conn.cursor()
+        cur.execute("""SELECT * FROM clinics where id = ? """,(int(clinic_id)))
+        return list(cur.fetchone())
+
---
+++ Repo/Repository.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+
+class _Repository:
+    def __init__(self):
+        self._conn = sqlite3.connect(".\\database.db")
+
+    def _close(self):
+        self._conn.commit()
+        self._conn.close()
+
+    def create_tables(self):
+        self._conn.executescript("""
+            CREATE TABLE IF NOT EXISTS clinics (
+                    id  INTEGER  PRIMARY KEY,
+                    location STRING NOT NULL,
+                    demand INTEGER  NOT NULL,
+                    logistic INTEGER  REFERENCES logistics(id)
+                );
+            CREATE TABLE IF NOT EXISTS logistics(
+                    id INTEGER PRIMARY KEY,
+                    name STRING NOT NULL,
+                    count_sent INTEGER NOT NULL,
+                    count_received INTEGER NOT NULL
+                );
+            CREATE TABLE IF NOT EXISTS suppliers (
+                    id      INTEGER     PRIMARY KEY,
+                    name    STRING      NOT NULL,
+                    logistic INTEGER REFERENCES logistics(id)
+                );
+            CREATE TABLE IF NOT EXISTS vaccines (
+                    id      INTEGER   PRIMARY KEY,
+                    date    DATE      NOT NULL,
+                    supplier INTEGER REFERENCES suppliers(id),
+                    quantity INTEGER  NOT NULL
+                );
+            """)
+        pass
+
+        def get_config_file(self, conn, config_file_path):
+
