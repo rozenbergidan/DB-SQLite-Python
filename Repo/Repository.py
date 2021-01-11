297f3eec00a22846ad660634a7f982e86c9f2aa5
rozenbergidan <idanroze@post.bgu.ac.il>
2021-01-11 17:28:49 +0200

    ..

--- Repo/Repository.py
+++ Repo/Repository.py
@@ -9,15 +9,16 @@ class _Repository:
     def __init__(self):
         self._conn = sqlite3.connect(".\\database.db")
         self.clean()
-        self.create_tables()
-        self.get_config_file("config.txt")
+        # self.create_tables()
+        # self.get_config_file("config.txt")
+        self.get_orders_file("orders.txt")
 
     def clean(self):
         cur = self._conn.cursor()
-        cur.execute("""DROP TABLE vaccines;""")
-        cur.execute("""DROP TABLE suppliers;""")
-        cur.execute("""DROP TABLE clinics;""")
-        cur.execute("""DROP TABLE logistics;""")
+        cur.execute("""DROP TABLE IF EXISTS vaccines;""")
+        cur.execute("""DROP TABLE IF EXISTS suppliers;""")
+        cur.execute("""DROP TABLE IF EXISTS clinics;""")
+        cur.execute("""DROP TABLE IF EXISTS logistics;""")
 
     def _close(self):
         self._conn.commit()
@@ -54,8 +55,7 @@ class _Repository:
         pass
 
     def get_config_file(self, config_file_path):
-        cur = self._conn.cursor()
-        with open(".\\"+config_file_path, "r",encoding ="utf-8") as configFile:
+        with open(".\\"+config_file_path, "r", encoding ="utf-8") as configFile:
             rows = configFile.read().split("\n")
             data = rows[0].split(",")
 
@@ -63,12 +63,6 @@ class _Repository:
             suppliers = (1+vaccines[1], vaccines[1]+int(data[1]))
             clinics = (1+suppliers[1],  suppliers[1]+int(data[2]))
             logistics = (1+clinics[1], clinics[1]+int(data[3]))
-
-            # print(vaccines)
-            # print(suppliers)
-            # print(clinics)
-            # print(logistics)
-
 
 
             for line in rows[logistics[0]:1+logistics[1]]:
@@ -88,8 +82,13 @@ class _Repository:
 
             pass
 
-
-
+    def get_orders_file(self, orders_file_path):
+        with open(".\\"+orders_file_path, "r", encoding ="utf-8") as ordersFile:
+            rows = ordersFile.read().split("\n")
+            for row in rows:
+                data = row.split(",")
+                print(data)
+        pass
 
 
 
