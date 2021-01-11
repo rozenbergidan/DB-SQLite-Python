5f814608b6c7d739e149215f3d904b6b3c6a8148
tsuri <tsurif@post.bgu.ac.il>
2021-01-11 17:38:29 +0200

    DTO

---
+++ ApplicationLayer/VaccineDTO.py
@@ -0,0 +1,6 @@
+class VaccineDTO:
+    def __init__(self, idd, date, supplier, quantity):
+        self.idd = idd
+        self.date = date
+        self.supplier = supplier
+        self.quantity = quantity
